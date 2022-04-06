391882da5d7ff307ba4e6fb0cc7febb462b570ff
Laurence <86679332+laureh90@users.noreply.github.com>
2022-04-06 13:29:36 -0400

    Delete exemple-km.js
    not required for the project

--- synthese/exemple-km.js
+++
@@ -1,9 +0,0 @@
-//exemple pour afficher minute et seconde séparer mais  avec km
-
-
-let metres = 25152
-
-let km = Math.floor(metres / 1000) //math.floor enleve les chiffre apres la virgule , math.round ca arrondi
-let m = metres % 1000
-
- //10 % 3 = 123 123 123 au lieu de 12345678910
